1d70e19b07d50aceb88d2fdab22a51215ca2d5b1
Soham <sohammokal420@gmail.com>
2024-02-28 14:19:58 +0530

    chatbot fix

--- app/src/main/java/com/example/newschatbot/API.java
+++ app/src/main/java/com/example/newschatbot/API.java
@@ -2,5 +2,5 @@ package com.example.newschatbot;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "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";
 }
