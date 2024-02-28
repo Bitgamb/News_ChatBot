9ec5dfa41aff445af1e5eb66cbc5e8f9745f7445
Soham <sohammokal420@gmail.com>
2024-02-28 13:40:33 +0530

    news display category added

--- app/src/main/java/com/example/newschatbot/NewsDisplay.java
+++ app/src/main/java/com/example/newschatbot/NewsDisplay.java
@@ -57,7 +57,7 @@ public class NewsDisplay extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         setContentView(R.layout.activity_news_display);
 
         swipeRefreshLayout = findViewById(R.id.srl);
